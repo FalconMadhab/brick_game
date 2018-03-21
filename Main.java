925cb58c02f4c10c84c576c20a15c22e9d1e09a3
FalconMadhab <32309032+FalconMadhab@users.noreply.github.com>
2018-03-21 19:25:45 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package brick;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame obj = new JFrame();
+		Gameplay game = new Gameplay();
+		obj.setBounds(10, 10, 700, 600);
+		obj.setTitle("Break the Bricks");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(game);
+	}
+
+}
